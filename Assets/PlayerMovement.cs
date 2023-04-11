03208b8f803bf5a5b8ea8562f1ce3ecc376507ad
bmauncho <bmauncho@gmail.com>
2023-04-11 23:47:30 +0300

    Bradley

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController charController;
+    private Vector3 playerVelocity;
+    public bool IsGrounded;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsGrounded = charController.isGrounded;
+        if(IsGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        charController.Move(movement*Time.deltaTime*playerSpeed);
+
+        if (movement != Vector3.zero)
+        {
+            gameObject.transform.forward = movement;
+        }
     }
 }
